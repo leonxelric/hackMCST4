d5af501eaa21962a67a70acf13a40a249c1ea876
LEON-LAPTOP\leonx <lxie21@mlschools.org>
2018-11-11 05:11:38 -0500

    position loops

--- hackMCST4 project/src/project/Position.java
+++ hackMCST4 project/src/project/Position.java
@@ -1,13 +1,24 @@
 package project;
 
 public class Position {
-	private int[] xVals;
-	private int[] yVals;
-	private int[] zVals;
+	private float[] xVals;
+	private float[] yVals;
+	private float[] zVals;
 	
-	public Position(int[] xV, int[] yV, int[] zV)
+	public Position(float[] rawXVals, float[] rawYVals, float[] rawZVals)
 	{
+		float xOrigin = rawXVals[0];
+		float yOrigin = rawYVals[0];
+		float zOrigin = rawZVals[0];
 		
+		for(int i = 0; i < rawXVals.length; i++)
+		{
+			rawXVals[0] -= xOrigin;
+			rawYVals[0] -= yOrigin;
+			rawZVals[0] -= zOrigin;
+		}
+		//for loop that loops through x vals and subtracts x origin from each
+		//ditto for y and z
 	}
 	
 	public boolean closeEnough(Position other)
@@ -15,17 +26,17 @@ public class Position {
 		return false;
 	}
 	
-	public int[] getXVals()
+	public float[] getXVals()
 	{
 		return xVals;
 	}
 	
-	public int[] getYVals()
+	public float[] getYVals()
 	{
 		return yVals;
 	}
 	
-	public int[] getZVals()
+	public float[] getZVals()
 	{
 		return zVals;
 	}
